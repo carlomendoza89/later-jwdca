6ecdbe227f0d1b1bd23d3ff009799ee638132aab
Carlo Mendoza <carlomendoza89@gmail.com>
2020-11-01 23:40:27 -0800

    first draft of full project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,118 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LandingPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from "./components/LandingPage";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    LandingPage,
+  },
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: $paragraph;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: $primary;
-  margin-top: 60px;
+  font-family: $paragraph;
+}
+p {
+  color: $secondary;
+}
+section {
+  background: $background;
+  padding-top: 50px;
+  padding-bottom: 80px;
+}
+.section-fading {
+  background: linear-gradient(rgb(250, 248, 245) 0%, rgb(255, 255, 255) 100%)
+}
+.section-header {
+  text-align: center;
+  margin: 16px 0 32px;
+}
+.into-header {
+  font-size: $xl-font-size;
+}
+.container {
+  max-width: 1200px;
+}
+.button {
+  color: $salmon;
+  border: 1px solid $salmon;
+  border-radius: 50px;
+  text-decoration: none;
+  padding: 10px 20px;
+  font-weight: $medium-font-weight;
+}
+.button:hover {
+  color: $background;
+  background: $salmon;
+  text-decoration: none;
+}
+.button .icon {
+  line-height: $paragraph-line_height;
+  height: $paragraph-size;
+  vertical-align: middle;
+}
+.xl-font {
+  font-family: $xl-font;
+  font-weight: $medium-font-weight;
+  font-size: $xl-font-size;
+  line-height: $xl-font-line_height;
+}
+h1 {
+  font-family: $header1;
+  font-weight: $medium-font-weight;
+  font-size: $header1-size;
+  line-height: $header1-line_height;
+}
+h2 {
+  font-family: $header2;
+  font-weight: $bold-font-weight;
+  font-size: $header2-size;
+  line-height: $header2-line_height;
+}
+h3 {
+  font-family: $header3;
+  font-weight: $regular-font-weight;
+  font-size: $header3-size;
+  line-height: $header3-line_height;
+}
+h4 {
+  font-family: $header4;
+  font-weight: $regular-font-weight;
+  font-size: $header4-size;
+  line-height: $header4-line_height;
+}
+h5 {
+  font-family: $header5;
+  font-weight: $regular-font-weight;
+  font-size: $header5-size;
+  line-height: $header5-line_height;
+}
+h6 {
+  font-family: $header6;
+  font-weight: $medium-font-weight;
+  font-size: $header6-size;
+  line-height: $header6-line_height;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+}
+p {
+  font-family: $paragraph;
+  font-weight: $regular-font-weight;
+  font-size: $paragraph-size;
+  line-height: $paragraph-line_height;
+}
+.poppins-bold {
+  font-family: "Poppins-Bold", "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+.salmon-text {
+  color: $salmon;
 }
 </style>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,200 @@
+<template>
+  <main>
+    <section class="section-fading">
+      <div class="container">
+        <h1 class="section-header into-header">
+          A social media marketing platform <br />
+          that <span class="poppins-bold">actually</span> helps you grow
+        </h1>
+        <div class="row">
+          <div class="col-md-6">
+            <img
+              src="../assets/Hp--visual--planner-f0bab8b1.png"
+              alt="Visual Planner Image"
+              style="max-width: 100%"
+            />
+          </div>
+          <div class="col-md-6">
+            <h6 class="salmon-text section">Visual Planner</h6>
+            <h1>Plan a month's worth of social media posts in minutes</h1>
+            <p>
+              Scheduling your social media content is as simple as
+              dragging-and-droppinf. No, serviously, Later will tell you your
+              unique Best Time to Post, so you can schedule in batches and
+              publish without a hitch.
+            </p>
+            <br />
+            <a class="button" href="https://later.com/schedule/"
+              >Visual Planner
+              <b-icon-chevron-right class="icon"></b-icon-chevron-right>
+            </a>
+            <img
+              class="decorative_icon"
+              src="../assets/square.svg"
+              alt="square"
+              style="position: absolute; right: 0; bottom: -140px"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="testimonials--section">
+      <div class="container">
+        <h6 class="section-header">What Our Customers Say</h6>
+        <div class="row justify-content-center customer_testimonial--row">
+          <div class="col-lg-4 customer_testimonial--container">
+            <div class="col-12 customer_testimonial">
+              <div class="row customer_testimonial-stars--row">
+                <img
+                  class="customer_testimonial-stars"
+                  src="../assets/stars-069fa221.svg"
+                  alt="5 Stars"
+                />
+              </div>
+              <div class="row customer_testimonial-review--row">
+                <p class="customer_testimonial-review">
+                  We've been using Later for a little over 2 years. The visual
+                  planner is one of the tools that has helped us become more
+                  efficient and it's an important part of our marketing toolkit
+                </p>
+                <br />
+                <br />
+              </div>
+              <div class="row customer_testimonial-profile">
+                <p class="customer_testimonial-profile--info">
+                  <img
+                    src="../assets/ryan-babenzien-188b1ec5.png"
+                    alt="Ryan Babenzien Pic"
+                  />
+                  <span class="customer_testimonial-profile--info__name"
+                    >Ryan Babenzien</span
+                  ><br />
+                  <span class="custoemr_testimonial-profile--info__title"
+                    >Founder and CEO of GREATS</span
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 customer_testimonial--container">
+            <div class="col-12 customer_testimonial">
+              <div class="row customer_testimonial-stars--row">
+                <img
+                  class="customer_testimonial-stars"
+                  src="../assets/stars-069fa221.svg"
+                  alt="5 Stars"
+                />
+              </div>
+              <div class="row customer_testimonial-review--row">
+                <p class="customer_testimonial-review">
+                  Later's powerful automation tools and helpful trainings have
+                  helped drive me business forward. My business has grown with
+                  the platform, taking my brand from a lowly wedding blog to a
+                  successful destination wedding business.
+                </p>
+                <br />
+                <br />
+              </div>
+              <div class="row customer_testimonial-profile">
+                <p class="customer_testimonial-profile--info">
+                  <img
+                    src="../assets/olivia-desantos-39b7b4c7.png"
+                    alt="Olivia DeSantos Pic"
+                  />
+                  <span class="customer_testimonial-profile--info__name"
+                    >Olivia DeSantos</span
+                  ><br />
+                  <span class="customer_testimonial-profile--info__title"
+                    >Wedding Planner, Nulyweds</span
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 customer_testimonial--container">
+            <div class="col-12 customer_testimonial">
+              <div class="row customer_testimonial-stars--row">
+                <img
+                  class="customer_testimonial-stars"
+                  src="../assets/stars-069fa221.svg"
+                  alt="5 Stars"
+                />
+              </div>
+              <div class="row customer_testimonial-review--row">
+                <p class="customer_testimonial-review">
+                  Later has enabled our team to focus on producing quality,
+                  engaging content by freeing up time that would have been spent
+                  on collecting user-generated assets, planning and scheduling.
+                </p>
+                <br />
+                <br />
+              </div>
+              <div class="row customer_testimonial-profile">
+                <p class="customer_testimonial-profile--info">
+                  <img
+                    src="../assets/me-undies-423a19b1.png"
+                    alt="MeUndies Logo"
+                  />
+                  <span class="customer_testimonial-profile--info__name"
+                    >Social Media Team</span
+                  ><br />
+                  <span class="customer_testimonial-profile--info__title"
+                    >MeUndies</span
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.decorative_icon {
+  position: absolute;
+}
+.testimonials--section {
+  padding-bottom: 60px;
+}
+.customer_testimonial--row .row {
+  margin: 20px 0;
+}
+.customer_testimonial {
+  background: white;
+  box-shadow: $shadow;
+  height: 90%;
+  padding: 30px 25px;
+}
+.customer_testimonial p {
+  font-size: $header5-size;
+}
+.row .customer_testimonial-stars--row {
+  margin: 0;
+}
+.customer_testimonial-profile--info {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  padding-left: 25px;
+  padding-right: 25px;
+  width: 100%;
+}
+.customer_testimonial-profile img {
+  width: 62px;
+  float: left;
+  margin-right: 12px;
+}
+.customer_testimonial-profile--info__name {
+  font-weight: $bold-font-weight;
+  color: $primary;
+}
+</style>
